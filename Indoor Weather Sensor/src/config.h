78b90c2f049e0da76b6c44eddbf36add77773d55
steffensc <schmelter.steffen@googlemail.com>
2022-06-16 12:43:14 +0200

    Create config.h

---
+++ Indoor Weather Sensor/src/config.h
@@ -0,0 +1,13 @@
+#pragma once 
+
+// WiFi Configuration //
+const char *WIFI_SSID     = "Your WiFi SSID";
+const char *WIFI_PASSWORD = "WiFiPassword";
+
+
+// Server Endpoint Configuration //
+const String SERVER_URL   = "http://ServerAress.com/temperature/write_data.php";
+const String SERVER_TOKEN = "TokenStringSetInServerAPI";
+
+// Device Configuration
+const String SENSOR_NO = "0"; // Watch out for keeping this unique
